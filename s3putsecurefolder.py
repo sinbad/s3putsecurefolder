sinbad/s3putsecurefolder

commit d135128a9ffd6bb0011ce46c7ac93adc42dc7a10

Log when we're simulating, and print warning about hash time on large files

s3putsecurefolder.py
@@ -113,6 +113,10 @@ if bucket is None:
         print 'Error, bucket ' + targetBucket + ' does not exist.'
         exit(-1) 
 print 'Bucket opened successfully.'
+if options.simulate:
+    print 'Simulation mode, not actually uploading data.'
+
+print 'Please be patient, hash calculations can take a few seconds on larger files.'
 
 # standardise path (removes any trailing slash & double slashes)
 sourceFolder = os.path.normpath(sourceFolder)

commit e6db35ad9c33f81efc52e5d3001ed56ee34b454f

Fix reversing of d135128 caused by taking customised copies from different servers

s3putsecurefolder.py
@@ -124,6 +124,10 @@ if bucket is None:
         print 'Error, bucket ' + targetBucket + ' does not exist.'
         exit(-1) 
 print 'Bucket opened successfully.'
+if options.simulate:
+    print 'Simulation mode, not actually uploading data.'
+
+print 'Please be patient, hash calculations can take a few seconds on larger files.'
 
 # standardise path (removes any trailing slash & double slashes)
 sourceFolder = os.path.normpath(sourceFolder)