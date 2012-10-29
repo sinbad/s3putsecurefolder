256012fb5a98f4c5bab9182284bcb3336b9f6c51
Steve Streeting <steve@stevestreeting.com>
2012-10-29 11:51:11 +0000

    Update copyright

--- s3putsecurefolder.py
+++ s3putsecurefolder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2009 Steve Streeting
+# Copyright (c) 2009-2012 Steve Streeting
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
